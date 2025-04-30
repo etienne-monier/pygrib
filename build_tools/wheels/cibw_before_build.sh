abd58fb70a1cd392ec3441da8a7f3c43b50f4fc0
Etienne Monier <etienne.monier@csgroup.eu>
2025-04-30 11:05:25 +0200

    test6

--- build_tools/wheels/cibw_before_build.sh
+++ build_tools/wheels/cibw_before_build.sh
@@ -4,12 +4,19 @@ set -euxo pipefail
 
 ECCODES_VERSION=2.41.0
 ECCODES_DIR=/usr/local
+cat /etc/*-release
+apt-get install libaec-dev
 
+# ECCODES INSTALLATION
+#
+
+# Get Source code
 STEM="eccodes-${ECCODES_VERSION}-Source"
 curl https://confluence.ecmwf.int/download/attachments/45757960/${STEM}.tar.gz\?api\=v2 \
   --output ${STEM}.tar.gz
 tar -xf "${STEM}.tar.gz"
 
+# Build and install
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${ECCODES_DIR} ../${STEM}
